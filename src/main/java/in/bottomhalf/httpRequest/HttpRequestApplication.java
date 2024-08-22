9aed86b81530c78e48113c2d04893defcac54237
BottomHalf <istiyaq.mi9@gmail.com>
2024-08-22 08:17:48 +0530

    new files

--- src/main/java/in/bottomhalf/httpRequest/HttpRequestApplication.java
+++ src/main/java/in/bottomhalf/httpRequest/HttpRequestApplication.java
@@ -2,8 +2,10 @@ package in.bottomhalf.httpRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HttpRequestApplication {
 
 	public static void main(String[] args) {
