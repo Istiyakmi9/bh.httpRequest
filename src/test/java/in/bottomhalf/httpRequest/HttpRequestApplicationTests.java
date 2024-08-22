78f41f10bd395032a778f40ccb30e32be4fb8a85
BottomHalf <istiyaq.mi9@gmail.com>
2024-08-22 07:43:00 +0530

    new files

---
+++ src/test/java/in/bottomhalf/httpRequest/HttpRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bottomhalf.httpRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
